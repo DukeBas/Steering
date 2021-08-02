3d3aed93b222911c642190fe1707add7f103ef11
DukeBas <35384117+DukeBas@users.noreply.github.com>
2021-08-03 00:12:18 +0200

    Color picker and saved settings

--- src/index.ts
+++ src/index.ts
@@ -5,36 +5,56 @@ import "./style.scss";
 // File used for global declaration
 
 declare global {
-    interface Window {
-        openSidebar: Function,
-        closeSidebar: Function,
-        saveCanvas: Function,
-        windowResized: Function,
-        settings: typeof settings,
-        toggleFadingLines: Function,
-    }
+  interface Window {
+    openSidebar: Function,
+    closeSidebar: Function,
+    saveCanvas: Function,
+    windowResized: Function,
+    settings: typeof settings,
+    initialLoad: Function, // called when the body loads, to update values based on previously saved settings
+    toggleFadingLines: Function,
+    changeColor: Function,
+  }
 }
 
 // global settings
 const settings = {
-    fadingLines: true,  // whether lines fade over time
-    lineLifetime: parseInt((document.getElementById('lifetime-slider') as HTMLInputElement).value),  // how many frames lines are drawn for
-    color: {
-        r: 255,
-        g: 0,
-        b: 0,
-    },
-    VehicleOptions : {
-        maxForce: 0.25,
-        maxSpeed: 9,
-        distanceForMaxSpeed: 100,
-        distanceToTargetToReach: 50,
-      }
+  fadingLines: true,  // whether lines fade over time
+  lineLifetime: parseInt((document.getElementById('lifetime-slider') as HTMLInputElement).value),  // how many frames lines are drawn for
+  color: {
+    r: 255,
+    g: 0,
+    b: 0,
+  },
+  VehicleOptions: {
+    maxForce: 0.25,
+    maxSpeed: 9,
+    distanceForMaxSpeed: 100,
+    distanceToTargetToReach: 50,
+  }
 }
 window.settings = settings;
 
 // Fading lines toggle button
 function toggleFadingLines() {
-    settings.fadingLines = !settings.fadingLines;
+  settings.fadingLines = !settings.fadingLines;
 }
 window.toggleFadingLines = toggleFadingLines;
+
+// Updating color
+function changeColor(col: string) { // input color as DOMString, in sevencharacter hexadecimal notation
+  const values = col.substring(1) // remove '#'
+    .split(/(.{2})/)    // split into chunks of 2 for each of the components
+    .filter(p => p);    // remove empty parts created by split
+  window.settings.color.r = parseInt(values[0], 16);
+  window.settings.color.g = parseInt(values[1], 16);
+  window.settings.color.b = parseInt(values[2], 16);
+}
+window.changeColor = changeColor;
+
+// updating based on previous values
+function initialLoad() {
+  document.getElementById('lifetime-value').innerHTML = (document.getElementById('lifetime-slider') as HTMLInputElement).value
+  changeColor((document.getElementById('color-picker') as HTMLInputElement).value);
+}
+window.initialLoad = initialLoad;
